f54d88f7338b98e2994f01dae3fad65591de1679
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2015-02-20 22:57:06 +0100

    Temp on the page

--- src/cljs/aladin/client.cljs
+++ src/cljs/aladin/client.cljs
@@ -7,21 +7,26 @@
 
 (enable-console-print!)
 
-(defn doweather [data]
-   (jqm/let-ajax [weather {:url (str "/weather/" 
-                                     (-> data .-coords .-latitude)
-                                     "/"
-                                     (-> data .-coords .-longitude))}]
-     (println weather)
-     (let [curtime (now)
-           forecast-from (parse (formatter "yyyy-MM-dd HH:mm:ss Z") 
-                                (str (:forecastTimeIso weather) " +0100"))]
-       (println
-         (nth (-> weather :parameterValues :TEMPERATURE)
-              (in-hours (interval forecast-from curtime)))))))
+(def weather-data (r/atom {}))
+
+(defn current-index [weather]
+  (let [curtime (now)
+        forecast-from (parse (formatter "yyyy-MM-dd HH:mm:ss Z") 
+                             (str (:forecastTimeIso weather) " +0100"))]
+    (in-hours (interval forecast-from curtime))))
+
+(defn weather-now []
+  [:h1 (nth (-> @weather-data :parameterValues :TEMPERATURE)
+            (current-index @weather-data))])
+
+(defn show-weather [data]
+   (jqm/let-ajax [weather {:url (str "/weather/" (-> data .-coords .-latitude) "/" (-> data .-coords .-longitude))}]
+     (reset! weather-data weather)
+     (r/render-component [weather-now]
+                         (.-body js/document))))
 
 (jqm/ready
   (let [geo (.-geolocation js/navigator)]
     (if geo
-      (.getCurrentPosition geo doweather)
+      (.getCurrentPosition geo show-weather)
       (println "GTFO"))))
