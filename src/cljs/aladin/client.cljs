73bbe8089ded1709f91c90779d109d0b79f8f4c1
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2015-02-21 20:01:44 +0100

    Some styles; images; code cleanup

--- src/cljs/aladin/client.cljs
+++ src/cljs/aladin/client.cljs
@@ -2,28 +2,40 @@
   (:require [reagent.core :as r]
             [cljs-time.format :refer [parse formatter]]
             [cljs-time.core :refer [now in-hours interval]]
+            [goog.string :as gstring]
             [jayq.core :refer [document-ready ajax]])
   (:require-macros [jayq.macros :as jqm]))
 
 (enable-console-print!)
 
-(def weather-data (r/atom {}))
+(defonce weather-data (r/atom {}))
 
-(defn current-index [weather]
+(defn update-weather-state [weather]
   (let [curtime (now)
         forecast-from (parse (formatter "yyyy-MM-dd HH:mm:ss Z") 
-                             (str (:forecastTimeIso weather) " +0100"))]
-    (in-hours (interval forecast-from curtime))))
+                             (str (:forecastTimeIso weather) " +0100"))
+        index (in-hours (interval forecast-from curtime))]
+    (reset! weather-data
+            (-> (:parameterValues weather)
+                (assoc :icons (mapcat (partial repeat 2) 
+                                      (:weatherIconNames weather)))
+                (assoc :current-index index)))))
+
+(defn current-value [data val-kw]
+  (nth (val-kw data)
+       (:current-index data)))
 
 (defn weather-now []
-  [:h1 (nth (-> @weather-data :parameterValues :TEMPERATURE)
-            (current-index @weather-data))])
+  [:h1 [:img {:src (str "/img/meteo/" (current-value @weather-data :icons) ".svg")}]
+   (gstring/format "%.1f" (current-value @weather-data :TEMPERATURE)) " °C" ])
 
 (defn show-weather [data]
-   (jqm/let-ajax [weather {:url (str "/weather/" (-> data .-coords .-latitude) "/" (-> data .-coords .-longitude))}]
-     (reset! weather-data weather)
+   (jqm/let-ajax [weather 
+                  {:url 
+                   (str "/weather/" (-> data .-coords .-latitude) "/" (-> data .-coords .-longitude))}]
+     (update-weather-state weather)
      (r/render-component [weather-now]
-                         (.-body js/document))))
+                         (.getElementById js/document "container"))))
 
 (jqm/ready
   (let [geo (.-geolocation js/navigator)]
